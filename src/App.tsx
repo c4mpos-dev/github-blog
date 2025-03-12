10b5e1baae7ccb384e7b60df7d2427bff82b52e8
Cauã Campos <campos.eet@gmail.com>
2025-03-12 17:15:57 -0300

    Config framework and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center bg-base-background">
+      <h1 className="text-base-subtitle text-3xl font-nunito font-bold">GitHub Blog</h1>
+    </div>
+  )
+}
