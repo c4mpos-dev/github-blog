3cbed11081a1e7463f9f5077eaeffc6c71023530
Cauã Campos <campos.eet@gmail.com>
2025-03-13 17:03:46 -0300

    Profile Card

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header(){
     return (
         <header className="relative shadow-lg shadow-black/40">
             <img src={bannerCover} className="w-full h-[296px] object-cover"></img>
-            <img src={logoGitHubBlog} className="absolute left-1/2 top-2/5 transform -translate-x-1/2 -translate-y-1/2" />
+            <img src={logoGitHubBlog} className="absolute left-1/2 top-3/8 transform -translate-x-1/2 -translate-y-1/2" />
         </header>
     );
 }
